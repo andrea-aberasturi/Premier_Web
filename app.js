3d6de587c720b75cb51bcf6ad0ab65ebef4be526
Andrea Aberasturi <aberasturiandrea@gmail.com>
2022-05-18 06:47:12 -0300

    agregue servidor modifique html form

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = requiere('express');
 const request = requiere('request');
+const https = requiere('https');
 
 const app = express();
+
+//Indico con el metodo get enviar un archivo desde el directorio raiz + la entrada html que quiero mostrar
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname +'entre.html')
+});
+
+
 
 app.listen(3000, ()=>{
     console.log('Server est marcchée')
